ec508729930fd249bd5849e4cbe4eea201f6d696
Ammaar Alam <ammaar@princeton.edu>
2026-04-13 00:07:12 -0400

    (bug) lazy-load SAC export from baselines

--- src/cos435_citylearn/baselines/__init__.py
+++ src/cos435_citylearn/baselines/__init__.py
@@ -1,4 +1,9 @@
 from cos435_citylearn.baselines.rbc import run_rbc
-from cos435_citylearn.baselines.sac import run_sac
+
+
+def run_sac(*args, **kwargs):
+    from cos435_citylearn.baselines.sac import run_sac as _run_sac
+
+    return _run_sac(*args, **kwargs)
 
 __all__ = ["run_rbc", "run_sac"]
